c14d20696a7ff7422e666ccda116506918fb0e6c
heidless <rob.lockhart@yahoo.co.uk>
2023-02-14 14:24:33 +0000

    set HOST for Docker

--- app/config/settings.py
+++ app/config/settings.py
@@ -105,8 +105,8 @@ DATABASES = {
         'NAME': 'hpfolio',
         'USER': 'heidless',
         'PASSWORD': 'havana11',
-        #'HOST': 'host.docker.internal',
-        'HOST': 'localhost',
+        'HOST': 'host.docker.internal',
+        #'HOST': 'localhost',
         'PORT': '', # leave blank so the default port is selected
 #        'PORT': '', # leave blank so the default port is selected
     }
